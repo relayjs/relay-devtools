7753d406a0e83f31468235c073f74141475fe319
Slava Kim <slv@fb.com>
2017-08-22 16:28:46 -0700

    Source code for RN runtime part of debugger
    Summary: Source code of the runtime part imported in RN to support the debugger communication
    
    Reviewed By: leebyron
    
    Differential Revision: D5664712
    
    fbshipit-source-id: f355b1dc25c5ba52cf8f22823f2d821c92935dbb

---
+++ lib.config.js
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './util/loadRegeneratorRuntime.js',
+    './util/setElectronEnv.js',
+    './lib.js',
+  ],
+  output: {
+    filename: 'index.js',
+    path: path.join(__dirname, '/lib'),
+    library: 'relay-devtools',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+      {
+        test: /\.less$/,
+        loader: 'style-loader!css-loader!less-loader',
+      },
+      {
+        test: /\.js$/,
+        include: __dirname,
+        loader: 'babel-loader',
+        exclude: /(node_modules)/,
+        options: {
+          presets: ['react', 'es2017', 'es2016', 'es2015', 'stage-2'],
+          plugins: [
+            'transform-regenerator',
+            'transform-class-properties',
+            'transform-object-rest-spread',
+          ],
+        },
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader?name=[name].[ext]',
+      },
+    ],
+  },
+  externals: {
+    http: 'commonjs http', // The polyfill version differ from node's http
+    buffer: 'root global', // Polyfilled version fails the instanceof check
+  },
+  devtool: 'eval',
+};
---
+++ lib.js
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ws from 'ws';
+const http = require('http');
+
+import './css/reset.css';
+
+import api from './api';
+import setupRedux from './redux/setupRedux';
+import App from './components/App';
+import { Bridge } from './react-native-runtime/bridge';
+
+export function start(node, port) {
+  startServer(port, wall => {
+    const bridge = new Bridge(wall);
+    const API = new api(bridge);
+    const store = setupRedux(API);
+
+    ReactDOM.render(<App store={store} />, node);
+  });
+}
+
+function startServer(port, cb) {
+  const httpServer = http.createServer();
+  const server = new ws.Server({ server: httpServer });
+
+  /* eslint-disable no-console */
+  server.on('connection', socket => {
+    console.log('new connection');
+    socket.onerrror = err => {
+      console.log('RN Connection Error', err.message);
+    };
+    socket.onclose = () => {
+      console.log('RN Connection Closed');
+    };
+
+    const listeners = [];
+    socket.onmessage = evt => {
+      const data = JSON.parse(evt.data);
+      listeners.forEach(fn => fn(data));
+    };
+
+    const wall = {
+      listen(fn) {
+        listeners.push(fn);
+      },
+      send(data) {
+        if (socket.readyState === socket.OPEN) {
+          socket.send(JSON.stringify(data));
+        }
+      },
+      disconnect() {
+        socket.close();
+      },
+    };
+
+    cb(wall);
+  });
+  httpServer.listen(port, () => {
+    console.log('listening on port', port);
+  });
+}
