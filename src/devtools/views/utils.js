d28c75a6f2f9959ba1b1b42aa48044e09861fff2
Jan Kassens <jan@kassens.net>
2019-08-12 15:36:53 -0400

    Remove React tests

--- src/devtools/views/utils.js
+++ src/devtools/views/utils.js
@@ -1,29 +1,5 @@
 // @flow
 
-// Keeping this in memory seems to be enough to enable the browser to download larger profiles.
-// Without this, we would see a "Download failed: network error" failure.
-let downloadUrl = null;
-
-export function downloadFile(filename: string, text: string): void {
-  const blob = new Blob([text], { type: 'text/plain;charset=utf-8' });
-
-  if (downloadUrl !== null) {
-    URL.revokeObjectURL(downloadUrl);
-  }
-
-  downloadUrl = URL.createObjectURL(blob);
-
-  const element = document.createElement('a');
-  element.setAttribute('href', downloadUrl);
-  element.setAttribute('download', filename);
-  element.style.display = 'none';
-  ((document.body: any): HTMLBodyElement).appendChild(element);
-
-  element.click();
-
-  ((document.body: any): HTMLBodyElement).removeChild(element);
-}
-
 export function truncateText(text: string, maxLength: number): string {
   const { length } = text;
   if (length > maxLength) {
--- src/utils.js
+++ src/utils.js
@@ -1,73 +1,5 @@
 // @flow
 
-import LRU from 'lru-cache';
-
-const cachedDisplayNames: WeakMap<Function, string> = new WeakMap();
-
-// On large trees, encoding takes significant time.
-// Try to reuse the already encoded strings.
-let encodedStringCache = new LRU({ max: 1000 });
-
-export function getDisplayName(
-  type: Function,
-  fallbackName: string = 'Anonymous'
-): string {
-  const nameFromCache = cachedDisplayNames.get(type);
-  if (nameFromCache != null) {
-    return nameFromCache;
-  }
-
-  let displayName;
-
-  // The displayName property is not guaranteed to be a string.
-  // It's only safe to use for our purposes if it's a string.
-  // github.com/facebook/react-devtools/issues/803
-  if (typeof type.displayName === 'string') {
-    displayName = type.displayName;
-  }
-
-  if (!displayName) {
-    displayName = type.name || fallbackName;
-  }
-
-  cachedDisplayNames.set(type, displayName);
-  return displayName;
-}
-
-let uidCounter: number = 0;
-
-export function getUID(): number {
-  return ++uidCounter;
-}
-
-export function utfDecodeString(array: Array<number>): string {
-  return String.fromCodePoint(...array);
-}
-
-export function utfEncodeString(string: string): Array<number> {
-  let cached = encodedStringCache.get(string);
-  if (cached !== undefined) {
-    return cached;
-  }
-
-  const encoded = new Array(string.length);
-  for (let i = 0; i < string.length; i++) {
-    encoded[i] = string.codePointAt(i);
-  }
-  encodedStringCache.set(string, encoded);
-  return encoded;
-}
-
-export function printOperationsArray(operations: Array<number>) {
-  // The first two values are always rendererID and rootID
-  const rendererID = operations[0];
-  const rootID = operations[1];
-
-  const logs = [`opertions for renderer:${rendererID} and root:${rootID}`];
-  logs.push('TODO');
-  console.log(logs.join('\n  '));
-}
-
 // Pulled from react-compat
 // https://github.com/developit/preact-compat/blob/7c5de00e7c85e2ffd011bf3af02899b63f699d3a/src/index.js#L349
 export function shallowDiffers(prev: Object, next: Object): boolean {
@@ -82,31 +14,4 @@ export function shallowDiffers(prev: Object, next: Object): boolean {
     }
   }
   return false;
-}
-
-export function getInObject(object: Object, path: Array<string | number>): any {
-  return path.reduce((reduced: Object, attr: string | number): any => {
-    if (typeof reduced === 'object' && reduced !== null) {
-      return reduced[attr];
-    } else if (Array.isArray(reduced)) {
-      return reduced[attr];
-    } else {
-      return null;
-    }
-  }, object);
-}
-
-export function setInObject(
-  object: Object,
-  path: Array<string | number>,
-  value: any
-) {
-  const length = path.length;
-  const last = path[length - 1];
-  if (object != null) {
-    const parent = getInObject(object, path.slice(0, length - 1));
-    if (parent) {
-      parent[last] = value;
-    }
-  }
 }
