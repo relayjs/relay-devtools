a46dfd10a198719ea94889d38b73e330dc656360
Jan Kassens <jan@kassens.net>
2019-08-12 16:33:32 -0400

    Upgrade prettier

--- src/__tests__/storeSerializer.js
+++ src/__tests__/storeSerializer.js
@@ -70,9 +70,7 @@ export function printStore(store, includeWeight = false) {
   // Make sure the pretty-printed test align with the Store's reported number of total rows.
   if (rootWeight !== store.numElements) {
     throw Error(
-      `Inconsistent Store state. Individual root weights (${rootWeight}) do not match total weight (${
-        store.numElements
-      })`
+      `Inconsistent Store state. Individual root weights (${rootWeight}) do not match total weight (${store.numElements})`
     );
   }
 
