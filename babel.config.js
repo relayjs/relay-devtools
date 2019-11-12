6976b6ae89e0c7e964dec9cf0def45ea30ddbef0
Jan Kassens <jan@kassens.net>
2019-11-12 10:39:54 -0500

    Add some missing copyright headers

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const chromeManifest = require('./shells/browser/chrome/manifest.json');
 const firefoxManifest = require('./shells/browser/firefox/manifest.json');
 
--- shells/dev/relay-app/index.js
+++ shells/dev/relay-app/index.js
@@ -1,4 +1,11 @@
-/** @flow */
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
 
 // This test harness mounts each test app as a separate root to test multi-root applications.
 
